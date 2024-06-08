35c7da525f0194ee3fe91bdd7e1f2e0b8677a47b
F0sya <nikitachmyria@gmail.com>
2024-06-08 17:13:14 +0300

    Fifth practice exercise is done.

--- cpp_pointers/main.cpp
+++ cpp_pointers/main.cpp
@@ -1,49 +1,47 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
-double* add(double* number_1, double* number_2) {
-	return (new double{ *number_1 + *number_2 });
-}
-double* subtraction(double* number_1, double* number_2) {
-	return (new double{ *number_1 - *number_2 });
-}
-double* multiply(double* number_1, double* number_2) {
-	return (new double{ *number_1 * *number_2 });
-}
-double* divide(double* number_1, double* number_2) {
-	return (*number_1 != 0 && *number_2 != 0) ? new double{ *number_1 / *number_2 } : nullptr;
-}
-int main() {
-	double* number_1 = new double;
-	double* number_2 = new double;
-	int* action = new int;
-	double* result = nullptr;
 
-	cout << "Enter the first number:"; cin >> *number_1;
-	cout << "Enter the second number:"; cin >> *number_2;
-	cout << "Enter an action(1-4):\n1 - Add\n2 - Subtraction\n3 - Multiply\n4 - Divide\n"; cin >> *action;
-	switch (*action) {
-	case 1:
-		result = add(number_1, number_2);
-		break;
-	case 2:
-		result = subtraction(number_1, number_2);
-		break;
-	case 3:
-		result = multiply(number_1, number_2);
-		break;
-	case 4:
-		result = divide(number_1, number_2);
-		break;
-	default:
-		cout << "Unidentified action!";
-		break;
-		return 0;
-	}
-	(result != nullptr) ? cout << "Result: " << *result << endl : cout << "The result value is null!" << endl;
-	delete number_1, number_2;
-	if (result != nullptr)
-	{
-		delete result;
-	}
-	return 0;
+template <typename T>
+T* createArray(int size) {
+    T* arr = new T[size];
+    for (int i = 0; i < size; i++) {
+        arr[i] = rand() % 255;
+    }
+    return arr;
+}
+
+template <typename T>
+void printArray(T* arr, int size) {
+    for (int i = 0; i < size; i++) {
+        cout << *(arr + i) << " ";
+    }
+    cout << endl;
+}
+template <typename T>
+T* sumArray(int* arr, int* size) {
+    int* sum = new int{ 0 };
+    for (int i = 0; i < *size; i++) {
+        *sum += *(arr + i);
+    }
+    return sum;
+}
+
+int main() {
+    srand(time(0));
+    int* size = new int;
+    int* result = nullptr;
+    cout << "Enter the size of the array: ";
+    cin >> *size;
+    int* arr = createArray<int>(*size);
+    printArray(arr, *size);
+
+    result = sumArray<int>(arr, size);
+
+    cout << "Summary:" << *result << endl;
+    delete[] arr;
+    delete size,result;
+ 
+
+    return 0;
 }

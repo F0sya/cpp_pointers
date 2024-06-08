60419b2dc7f6943ef9a776024a151e7aa2bc2016
F0sya <nikitachmyria@gmail.com>
2024-06-08 16:14:41 +0300

    First practice exercise is done.

---
+++ cpp_pointers/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+
+int* bigger(int* number_1, int* number_2) {
+	if (*number_1 == *number_2) {
+		cout << "They equal" << endl;
+		return 0;
+	}
+	else {
+		return (*number_1 < *number_2) ? number_2 : number_1;
+	}
+}
+
+int main() {
+	int* number_1 = new int;
+	int* number_2 = new int;
+	int* result = new int;
+
+	cout << "Enter the first number:"; cin >> *number_1;
+	cout << "Enter the second number:"; cin >> *number_2;
+	result = bigger(number_1, number_2);
+	cout << *result << endl;
+	return 0;
+}
